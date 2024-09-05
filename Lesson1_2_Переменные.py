7a775def1dc08e0320784c020e900619dd856594
Vladimir-Bur <bvi1969@gmail.com>
2024-09-05 18:40:50 +0300

    Add files via upload

---
+++ Lesson1_2_Переменные.py
@@ -0,0 +1,5 @@
+Quantity_of_completed_homework = 12
+Quantity_of_hours_spent = 1.5
+Course_name = 'Python'
+Time_for_one_lesson = Quantity_of_hours_spent / Quantity_of_completed_homework
+print('Курс:',Course_name,', всего задач:',Quantity_of_completed_homework,', затрачено часов:',Quantity_of_hours_spent,', среднее время выполнения',Time_for_one_lesson,' часа.')
