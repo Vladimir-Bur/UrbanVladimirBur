6e21526abd5bb012e9d0a283e24fe442fa7b9565
Vladimir-Bur <bvi1969@gmail.com>
2024-09-09 20:04:52 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,10 @@
+immutable_var = 1,3,5,'modul_1_5', 7.0, True
+print(immutable_var)
+immutable_var = [1,3,5,'modul_1_5'], 7.0, True
+print(immutable_var)
+immutable_var[0][3] = 77
+print(immutable_var)
+mutable_list = [1,3,5,'modul_1_5', 7.0, True]
+print(mutable_list)
+mutable_list[3] = 4
+print(mutable_list)
