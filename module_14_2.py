afbed6e1831486ae86abe4a0cfd068cde431d1aa
Vladimir-Bur <bvi1969@gmail.com>
2025-01-16 16:29:37 +0300

    Add files via upload

---
+++ module_14_2.py
@@ -0,0 +1,50 @@
+import sqlite3
+
+connection = sqlite3.connect('not_telegram.db')
+cursor = connection.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users(
+id INTEGER PRIMARY KEY, 
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+age INTEGER,
+balance INTEGER NOT NULL)
+''')
+
+# Код из предыдущего задания
+
+#for i in range(1, 11):
+#    cursor.execute('INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)',
+#                   (f'User{i}',f'example{i}@gmail.com', f'{i * 10}', '1000'))
+
+#n = 1
+#for b in range(5):
+#    cursor.execute('UPDATE Users SET balance = ? WHERE username = ?', (500, f'User{n}'))
+#    n += 2
+
+#User_Del = 1
+#for d in range(4):
+#    cursor.execute('DELETE FROM Users WHERE id = ?', (f'{User_Del}',))
+#    User_Del += 3
+
+#cursor.execute('SELECT username, email, age, balance FROM Users WHERE age != ?', (60, ))
+#users = cursor.fetchall()
+#for user in users:
+#    print(f'Имя: {user[0]} | Почта: {user[1]} | Возраст: {user[2]} | Баланс: {user[3]}')
+
+# Удаление пользователя с id=6
+cursor.execute('DELETE FROM Users WHERE id = ?', (6,))
+
+# Подсчёт кол-ва всех пользователей
+cursor.execute('SELECT COUNT(*) FROM Users')
+total_users = cursor.fetchone()[0]
+
+# Подсчёт суммы всех балансов
+cursor.execute('SELECT SUM(balance) FROM Users')
+all_balances = cursor.fetchone()[0]
+
+print(all_balances / total_users)
+
+connection.commit()
+connection.close()
